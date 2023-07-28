5069bf0c51d3d33276e1ecdc17a8a19182ddf783
qiq2i <qiq2i@qq.com>
2023-07-28 09:38:01 +0800

    初始化

---
+++ src/main/java/qiq2i/template/TemplateModClient.java
@@ -0,0 +1,10 @@
+package qiq2i.template;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TemplateModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
